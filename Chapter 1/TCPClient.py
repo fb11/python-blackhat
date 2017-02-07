489769b88b7c2596202828764751410a4e58762f
Nate <mauvehead@gmail.com>
2017-02-07 11:05:56 -0600

    TCPClient, TCPServer and most of bhpnet.py

--- Chapter 1/TCPClient.py
+++ Chapter 1/TCPClient.py
@@ -1,8 +1,8 @@
 # TCP Client - ch1
 import socket
 
-target_host = "attrition.org"
-target_port = 80
+target_host = "127.0.0.1"
+target_port = 9999
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
