8c86f79ba5927ca0b1f7460bac65d7aa5af76d93
Nate <mauvehead@gmail.com>
2017-03-28 10:26:34 -0500

    Add chap3 sniffer.py

---
+++ Chapter 3/sniffer.py
@@ -0,0 +1,30 @@
+import socket
+import os
+
+# host to lisen on
+host = 0.0.0.0
+
+# create a raw socket and bind it to the public interface
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host, 0))
+
+# we want the IP headers included in the apture
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# if we're using Windows, we need to send an IOCTL
+# to set promiscuous mode
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+# read in a single packet
+print sniffer.recvrom(65565)
+
+# if we're using Winows, turn off promiscuous mode
+if os.name = "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
